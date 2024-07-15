ab3c172b3cf55ec7e16335aa97d75bd43aa308f6
Deep Prakash Dewanji <deepprakash345@gmail.com>
2024-07-15 16:05:35 +0530

    Update Custom functions on Mon Jul 15 2024 16:05:35 GMT+0530 (India Standard Time)

---
+++ functions.js
@@ -0,0 +1,28 @@
+/**
+ * Calculate Total Principal
+ * @name getPrincipalAmount calculates total principal
+ * @param {number} sipValue
+ * @param {number} yearsOfInvestment
+ * @return {number}
+ */
+function getPrincipalAmount(sipValue, yearsOfInvestment) {
+    return sipValue*yearsOfInvestment*12;
+}
+
+/**
+ * Calculate Total Investment Value
+ * @name getTotalInvestmentValue calculates total investment value
+ * @param {number} sipValue
+ * @param {number} yearsOfInvestment
+ * @param {number} interestRate
+ * @return {number}
+ */
+function getTotalInvestmentValue(sipValue, yearsOfInvestment, interestRate) {
+	let i=interestRate/1200;
+    let n=yearsOfInvestment*12;
+	return sipValue*(Math.pow(1+i,n)-1)*((1+i)/i)
+}
+
+
+// eslint-disable-next-line import/prefer-default-export
+export { getPrincipalAmount,getTotalInvestmentValue };
