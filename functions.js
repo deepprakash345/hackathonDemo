7423405950c2fd26c41059876239fa96fc5abf69
Deep Prakash Dewanji <deepprakash345@gmail.com>
2024-07-15 11:25:01 +0530

    Update Custom functions

--- functions.js
+++ functions.js
@@ -20,6 +20,30 @@ function validateLength(str, length) {
     return str.length==length;
 }
 
+/**
+ * Calculate Total Principal
+ * @name getPrincipalAmount calculates total principal
+ * @param {number} sipValue 
+ * @param {number} yearsOfInvestment 
+ * @return {number} 
+ */
+function getPrincipalAmount(sipValue, yearsOfInvestment) {
+    return sipValue*yearsOfInvestment*12;
+}
+
+/**
+ * Calculate Total Investment Value
+ * @name getTotalInvestmentValue calculates total investment value
+ * @param {number} sipValue 
+ * @param {number} yearsOfInvestment
+ * @param {number} interestRate
+ * @return {number} 
+ */
+function getTotalInvestmentValue(sipValue, yearsOfInvestment, interestRate) {
+	let i=interestRate/1200;
+	return sipValue*(Math.pow(1+i,yearsOfInvestment)-1)*((1+i)/i)
+}
+
 
 // eslint-disable-next-line import/prefer-default-export
-export { getFullName,validateLength };
+export { getFullName,validateLength,getPrincipalAmount,getTotalInvestmentValue };
